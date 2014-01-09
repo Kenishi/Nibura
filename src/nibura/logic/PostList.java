11766fe9414c5a81fa1c53f283edbc1867672352
Kenishi <kenishi86@gmail.com>
2014-01-09 09:45:39 +0900

    Revert "Added more code for PostLink"
    This reverts commit 034c34668f6a69afe62999b8c4f5cada67b73ead.

---
+++ src/nibura/logic/PostList.java
@@ -0,0 +1,11 @@
+package nibura.logic;
+
+import java.util.ArrayList;
+
+public class PostList {
+	private ArrayList<PostLink> postList = null;
+	
+	public PostList() {
+		postList = new ArrayList<PostLink>();
+	}
+}
